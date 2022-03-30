d05f0800ecd298148767649469b2776e585b6d26
Inathi Mbomvu <inathim@Inathis-MacBook-Pro.local>
2022-03-30 21:10:36 +0200

    Init

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#Intall Brew 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew install spotify
+brew install discord
+brew install google-drive
+brew install microsoft-outlook 
+brew install git
+
+
+brew install --cask slack
+brew install --cask adobe-acrobat-reader 
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
